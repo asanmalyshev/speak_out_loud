44bc65275b710f311ab43498008b6e6276ef3f42
Alexander Malyshev <a.san.malyshev@mail.ru>
2021-05-27 02:16:48 +0300

    remove odd param language in launch file

--- src/speak_out_loud_action_srv.cpp
+++ src/speak_out_loud_action_srv.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 typedef actionlib::SimpleActionServer<speak_out_loud::SpeakAction> Server;
 
-void execute(const speak_out_loud::SpeakGoalConstPtr& goal, Server* as, string lang, string default_voice) 
+void execute(const speak_out_loud::SpeakGoalConstPtr& goal, Server* as, string default_voice) 
 {
   speak_out_loud::SpeakResult result;
   
@@ -25,27 +25,10 @@ void execute(const speak_out_loud::SpeakGoalConstPtr& goal, Server* as, string l
 
   string module = "rhvoice";
   string voice = default_voice ;
-  // string voice_normal;
-  // string voice_important;
 
-  // if (lang == "en"){
-  //   voice_normal = "slt";
-  //   voice_important = "alan";
-  // }
-  // else{
-  //   voice_normal = "elena";
-  //   voice_important = "aleksandr";
-  // }
-
-	// cout << "Text to say: " +  str_to_play << endl;
   const char* client_name = ros::this_node::getName().c_str();
   SPDConnection* conn = spd_open(client_name, NULL, NULL, SPD_MODE_SINGLE);
   spd_set_output_module(conn, module.c_str());
-
-  // if (priority == SPD_IMPORTANT) 
-  //   spd_set_synthesis_voice(conn, voice_important.c_str());
-  // else
-  //   spd_set_synthesis_voice(conn, voice_normal.c_str());
 
   spd_set_synthesis_voice(conn, voice.c_str());
 
@@ -59,14 +42,11 @@ int main(int argc, char** argv)
 {
   ros::init(argc, argv, "speak_out_srv");
   ros::NodeHandle n;
-  string lang;
   string default_voice;
 
-  n.param<std::string>("speak_out_loud_srv/language", lang, "ru");
   n.param<std::string>("speak_out_loud_srv/default_voice", default_voice, "elena");
-	cout << "Language to use: " +  lang << endl;
 	cout << "Default voice: " +  default_voice << endl;
-  Server server(n, "speak_out_loud", boost::bind(&execute, _1, &server, lang, default_voice), false);
+  Server server(n, "speak_out_loud", boost::bind(&execute, _1, &server, default_voice), false);
   server.start();
   ros::spin();
   return 0;
