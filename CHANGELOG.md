8fce0899328171aa6c287c3a2dba5a47c24c3719
Alexander Malyshev <a.san.malyshev@mail.ru>
2021-09-14 02:16:09 +0300

    wip

---
+++ CHANGELOG.md
@@ -0,0 +1,19 @@
+# CHANGELOG FILE
+
+## DEV (2021.09.04)
+
+### Added
+  - in launch file there's a param to change default voice. Voices must be installed (see installation section);
+  - /do_i_say topic: whether something is saying or not.
+### Changed
+  - now there's only one default voice for all message priorities - female one;
+  - multithread mode is used;
+### Fixed
+  - add ns naming, followed by topic changes
+
+---
+
+## v1.0.0 (2021.05.27)
+Init package version.
+Basic functions
+
--- src/speak_out_loud_action_srv.cpp
+++ src/speak_out_loud_action_srv.cpp
@@ -4,6 +4,7 @@
 #include <actionlib/server/simple_action_server.h>
 #include "speak_out_loud/SpeakAction.h"
 #include "speak_out_loud/SpeakResult.h"
+#include "std_msgs/Bool.h"
 
 #include "libspeechd.h"
 
@@ -13,7 +14,22 @@ using namespace std;
  * Actionlib server to sound text sent as a service goal
  */
 
+std_msgs::Bool msg_do_i_say;
+ros::Publisher do_i_say_pub;
+
 typedef actionlib::SimpleActionServer<speak_out_loud::SpeakAction> Server;
+
+void end_of_speech(size_t msg_id, size_t client_id, SPDNotificationType type)
+{
+  msg_do_i_say.data = false;
+  do_i_say_pub.publish(msg_do_i_say);
+}
+
+void begin_of_speech(size_t msg_id, size_t client_id, SPDNotificationType type)
+{
+  msg_do_i_say.data = true;
+  do_i_say_pub.publish(msg_do_i_say);
+}
 
 void execute(const speak_out_loud::SpeakGoalConstPtr& goal, Server* as, string default_voice) 
 {
@@ -33,22 +49,30 @@ void execute(const speak_out_loud::SpeakGoalConstPtr& goal, Server* as, string d
   }
 
   const char* client_name = ros::this_node::getName().c_str();
-  SPDConnection* conn = spd_open(client_name, NULL, NULL, SPD_MODE_SINGLE);
+  // SPDConnection* conn = spd_open(client_name, NULL, NULL, SPD_MODE_SINGLE);
+  SPDConnection* conn = spd_open(client_name, NULL, NULL, SPD_MODE_THREADED);
   spd_set_output_module(conn, module.c_str());
-
   spd_set_synthesis_voice(conn, voice.c_str());
+  spd_set_notification_on(conn, SPD_BEGIN);
+  spd_set_notification_on(conn, SPD_END);
+  spd_set_notification_on(conn, SPD_CANCEL);
+
+  conn->callback_end = conn->callback_cancel = end_of_speech;
+  conn->callback_begin = begin_of_speech;
 
   spd_say(conn, priority, str_to_play.c_str());
-  spd_close(conn);
+  // spd_close(conn);
 
   as->setSucceeded(result);
 }
+
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "speak_out_srv");
   ros::NodeHandle n;
   string default_voice;
+  do_i_say_pub = n.advertise<std_msgs::Bool>("do_i_say", 10);
 
   n.param<std::string>("speak_out_loud_srv/default_voice", default_voice, "elena");
 	cout << "Default voice: " +  default_voice << endl;
